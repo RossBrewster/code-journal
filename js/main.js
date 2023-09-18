25882c5dad81b24665c672e2ccda8717763315bd
Ross Brewster <ross.brewster14@gmail.com>
2023-09-18 22:26:43 +0000

    Use array of the data model to store entry objects instead of new array.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const $image = document.querySelector('.url');
 const $imgUrlInput = document.querySelector('#photo-url');
 const $titleInput = document.querySelector('#entry-title');
 const $notesInput = document.querySelector('#notes');
-const entries = [];
 
 $imgUrlInput.addEventListener('input', handleUrlInput);
 
@@ -23,7 +22,7 @@ function handleSubmission(e) {
   };
   newEntry.entryId = data.nextEntryId;
   data.nextEntryId++;
-  entries.unshift(newEntry);
+  data.entries.unshift(newEntry);
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
   $imgUrlInput.value = '';
   $titleInput.value = '';
