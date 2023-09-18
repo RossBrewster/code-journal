05cce02731cc76cd3fe08ca9494ce990ee505bc4
Ross Brewster <ross.brewster14@gmail.com>
2023-09-18 22:04:24 +0000

    Add 'submit' event listener to store inputs into an array of objects and reset form. #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 const $image = document.querySelector('.url');
 const $imgUrlInput = document.querySelector('#photo-url');
+const $titleInput = document.querySelector('#entry-title');
+const $notesInput = document.querySelector('#notes');
+const entries = [];
 
 $imgUrlInput.addEventListener('input', handleUrlInput);
 
 function handleUrlInput(e) {
   $image.setAttribute('src', $imgUrlInput.value);
+}
+
+const $entry = document.querySelector('.journal-entry');
+
+$entry.addEventListener('submit', handleSubmission);
+
+function handleSubmission(e) {
+  e.preventDefault();
+  const newEntry = {
+    title: $titleInput.value,
+    image: $imgUrlInput.value,
+    notes: $notesInput.value,
+  };
+  newEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  entries.unshift(newEntry);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $imgUrlInput.value = '';
+  $titleInput.value = '';
+  $notesInput.value = '';
 }
